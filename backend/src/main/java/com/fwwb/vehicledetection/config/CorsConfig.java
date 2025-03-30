fdd110ef235fd0f92494e032bcf9b6295fbbb0e0
BH Y <hesskuro@gmail.com>
2025-03-30 15:40:01 +0800

    Cors显式定义为5173端口

--- backend/src/main/java/com/fwwb/vehicledetection/config/CorsConfig.java
+++ backend/src/main/java/com/fwwb/vehicledetection/config/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 // 如果需要允许凭证，不能使用 "*"，建议明确指定允许的域名（比如 http://localhost:5173），或者使用 allowedOriginPatterns：
-                .allowedOriginPatterns("*")
+                .allowedOrigins("http://localhost:5173")
                 // 针对允许的方法
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
